95ba9edb1cd124a76e1e1c9b83e873bc533b53a5
Cano <mcano4216@greenville.k12.sc.us>
2018-03-01 09:34:16 -0500

    started placing walls

--- maze_with_scoring.py
+++ maze_with_scoring.py
@@ -41,11 +41,13 @@ score2 = 0
 
 
 # make walls
-wall1 =  [300, 275, 200, 25]
-wall2 =  [400, 450, 200, 25]
-wall3 =  [100, 100, 25, 200]
+wall1 =  [200, 0, 600, 25]
+wall2 =  [0, 0, 100, 25]
+wall3 =  [200, 575, 600, 25]
+wall4 =  [0, 575, 100, 25]
+wall5 =  [100, 100, 25, 200]
 
-walls = [wall1, wall2, wall3]
+walls = [wall1, wall2, wall3, wall4, wall5]
 
 # Make coins
 coin1 = [300, 500, 25, 25]
@@ -55,7 +57,29 @@ coin3 = [150, 150, 25, 25]
 coins = [coin1, coin2, coin3]
 
 
+
+# Fonts
+MY_FONT = pygame.font.Font(None, 50)
+
+
+# stages
+START = 0
+PLAYING = 1
+END = 2
+
+def setup():
+    global block_pos, block_vel, size, stage
+    
+    block_pos = [375, 275]
+    block_vel = [0, 0]
+    size = 50
+
+    stage = START
+
 # Game loop
+
+setup()
+
 win = False
 done = False
 
@@ -66,93 +90,118 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
 
-    pressed = pygame.key.get_pressed()
+        elif event.type == pygame.KEYDOWN:
 
-    up1 = pressed[pygame.K_UP]
-    down1 = pressed[pygame.K_DOWN]
-    left1 = pressed[pygame.K_LEFT]
-    right1 = pressed[pygame.K_RIGHT]
+            if stage == START:
+                if event.key == pygame.K_SPACE:
+                    stage = PLAYING
+            
+            elif stage == PLAYING:
 
-    up2 = pressed[pygame.K_w]
-    down2 = pressed[pygame.K_s]
-    left2 = pressed[pygame.K_a]
-    right2 = pressed[pygame.K_d]
+                pressed = pygame.key.get_pressed()
 
-    
-    if left1:
-        vel1[0] = -player1_speed
-    elif right1:
-        vel1[0] = player1_speed
-    else:
-        vel1[0] = 0
-    
-    
-    if left2:
-        vel2[0] = -player2_speed
-    elif right2:
-        vel2[0] = player2_speed
-    else:
-        vel2[0] = 0
+                up1 = pressed[pygame.K_UP]
+                down1 = pressed[pygame.K_DOWN]
+                left1 = pressed[pygame.K_LEFT]
+                right1 = pressed[pygame.K_RIGHT]
 
-    
-    if up1:
-        vel1[1] = -player1_speed
-    elif down1:
-        vel1[1] = player1_speed
-    else:
-        vel1[1] = 0
-    
-    
-    if up2:
-        vel2[1] = -player2_speed
-    elif down2:
-        vel2[1] = player2_speed
-    else:
-        vel2[1] = 0
-
-
-        
-        
-    # Game logic (Check for collisions, update points, etc.)
-    ''' move the player in horizontal direction '''
-    player1[0] += vel1[0]
-    player2[0] += vel2[0]
-
-    ''' resolve collisions horizontally '''
-    for w in walls:
-        if intersects.rect_rect(player1, w):        
-            if vel1[0] > 0:
-                player1[0] = w[0] - player1[2]
-            elif vel1[0] < 0:
-                player1[0] = w[0] + w[2]
-
-        if intersects.rect_rect(player2, w):        
-            if vel2[0] > 0:
-                player2[0] = w[0] - player2[2]
-            elif vel2[0] < 0:
-                player2[0] = w[0] + w[2]
-
-
-    ''' move the player in vertical direction '''
-    player1[1] += vel1[1]
-    player2[1] += vel2[1]
-    
-    ''' resolve collisions vertically '''
-    for w in walls:
-        if intersects.rect_rect(player1, w):                    
-            if vel1[1] > 0:
-                player1[1] = w[1] - player1[3]
-            if vel1[1]< 0:
-                player1[1] = w[1] + w[3]
-
+                up2 = pressed[pygame.K_w]
+                down2 = pressed[pygame.K_s]
+                left2 = pressed[pygame.K_a]
+                right2 = pressed[pygame.K_d]
 
                 
-        if intersects.rect_rect(player2, w):                    
-            if vel2[1] > 0:
-                player2[1] = w[1] - player2[3]
-            if vel2[1]< 0:
-                player2[1] = w[1] + w[3]
+                if left1:
+                    vel1[0] = -player1_speed
+                elif right1:
+                    vel1[0] = player1_speed
+                else:
+                    vel1[0] = 0
+                
+                
+                if left2:
+                    vel2[0] = -player2_speed
+                elif right2:
+                    vel2[0] = player2_speed
+                else:
+                    vel2[0] = 0
 
+                
+                if up1:
+                    vel1[1] = -player1_speed
+                elif down1:
+                    vel1[1] = player1_speed
+                else:
+                    vel1[1] = 0
+                
+                
+                if up2:
+                    vel2[1] = -player2_speed
+                elif down2:
+                    vel2[1] = player2_speed
+                else:
+                    vel2[1] = 0
+
+
+            elif stage == END:
+                if event.key == pygame.K_SPACE:
+                    setup()
+
+                    
+        
+    # Game logic (Check for collisions, update points, etc.)
+        
+    if stage == PLAYING:
+        ''' move the player in horizontal direction '''
+        player1[0] += vel1[0]
+        player2[0] += vel2[0]
+        
+        ''' move the player in vertical direction '''
+        player1[1] += vel1[1]
+        player2[1] += vel2[1]
+        
+
+
+        ''' resolve collisions horizontally '''
+        for w in walls:
+            if intersects.rect_rect(player1, w):        
+                if vel1[0] > 0:
+                    player1[0] = w[0] - player1[2]
+                elif vel1[0] < 0:
+                    player1[0] = w[0] + w[2]
+
+            if intersects.rect_rect(player2, w):        
+                if vel2[0] > 0:
+                    player2[0] = w[0] - player2[2]
+                elif vel2[0] < 0:
+                    player2[0] = w[0] + w[2]
+
+
+        ''' resolve collisions vertically '''
+        for w in walls:
+            if intersects.rect_rect(player1, w):                    
+                if vel1[1] > 0:
+                    player1[1] = w[1] - player1[3]
+                if vel1[1]< 0:
+                    player1[1] = w[1] + w[3]
+
+
+                    
+            if intersects.rect_rect(player2, w):                    
+                if vel2[1] > 0:
+                    player2[1] = w[1] - player2[3]
+                if vel2[1]< 0:
+                    player2[1] = w[1] + w[3]
+
+        ''' end game on wall collision '''
+        '''
+        if block_pos[0] < 0 or block_pos[0] > 950 or \
+           block_pos[1] < 0 or block_pos[1] > 650:
+            stage = END
+
+        '''
+            
+     
 
     #''' here is where you should resolve player collisions with screen edges '''
 
@@ -165,13 +214,17 @@ while not done:
     bottom = player1[1] + player1[3]
 
     ''' if the block is moved completely off of the window, reposition it on the other side '''
+
+    
     if left > WIDTH:
         player1[0] = 0 - player1[2]
+        
     elif right < 0:
         player1[0] = WIDTH
-        
+       
     if bottom < 0:
         player1[1] = HEIGHT
+        
     elif top > HEIGHT:
         player1[1] = 0 - player1[3]
 
@@ -218,6 +271,22 @@ while not done:
         font = pygame.font.Font(None, 48)
         text = font.render("You Win!", 5, GREEN)
         screen.blit(text, [400, 200])
+
+
+        
+    ''' begin/end game text '''
+    if stage == START:
+        text1 = MY_FONT.render("Block", True, WHITE)
+        text2 = MY_FONT.render("(Press SPACE to play.)", True, WHITE)
+        screen.blit(text1, [350, 150])
+        screen.blit(text2, [225, 200])
+    elif stage == END:
+        text1 = MY_FONT.render("Game Over", True, WHITE)
+        text2 = MY_FONT.render("(Press SPACE to restart.)", True, WHITE)
+        screen.blit(text1, [310, 150])
+        screen.blit(text2, [210, 200])
+
+
 
     
     # Update screen (Actually draw the picture in the window.)
